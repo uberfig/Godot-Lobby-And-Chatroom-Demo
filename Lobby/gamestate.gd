b6bb473c9faedbf9545f455661792ff6ff162d0e
uberfig <aidan.gifford04@gmail.com>
2021-08-04 13:43:52 -0400

    different port

--- Lobby/gamestate.gd
+++ Lobby/gamestate.gd
@@ -10,7 +10,7 @@ extends Node
 # Default game server port. Can be any number between 1024 and 49151.
 # Not on the list of registered or common ports as of November 2020:
 # https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
-const DEFAULT_PORT = 42069
+const DEFAULT_PORT = 43666
 
 # Max number of players.
 const MAX_PEERS = 6
